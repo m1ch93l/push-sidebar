92bbad0f635724a85fab1edcf30145f118f2dbe1
Michael B. Bañaria <91449532+m1ch93l@users.noreply.github.com>
2022-12-21 10:54:29 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+  </head>
+  <body>
+    <nav>
+        M1ch93lTech
+    </nav>
+    <section>
+        <input type="checkbox" id="check">
+        <label for="check">
+        <i class="fas fa-bars" id="btn"></i>
+        <i class="fas fa-times" id="cancel"></i>
+        </label>
+        <div class="sidebar">
+        <header>My Menu</header>
+        <a href="#" class="active">
+            <span>Dashboard</span>
+        </a>
+        </div>
+
+        <div class="content">
+            this is my content
+        </div>
+    </section>
+</body>
+</html>
